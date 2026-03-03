b2342e99ffae0672dbc8c8492576ad8f0b4e5297
eternasuno <22316214+eternasuno@users.noreply.github.com>
2026-03-03 05:48:15 +0000

    rm exist app

--- libs/twc.tsx
+++
@@ -1,6 +0,0 @@
-import { createTwc, cx } from 'react-twc';
-import { twMerge } from 'tailwind-merge';
-
-export const compose: typeof cx = (...inputs) => twMerge(cx(inputs));
-
-export const twc = createTwc({ compose });
